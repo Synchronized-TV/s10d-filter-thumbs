5fa857fc2a0cf0cf3bd43e42fdcd54d2f858e0fc
jide <jide.fr@gmail.com>
2020-04-09 15:42:32 +0000

    Add diff option

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const filter = require('./filter');
 const query = require('./query');
 const sort = require('./sort');
 
-async function getThumbs({ request, videoId, args, fields = [] }) {
+async function getThumbs({ request, videoId, args, fields = [], diff }) {
   const items = await query(request, videoId, args, fields);
   const filteredItems = filter(args, items);
-  const sortedFilteredItems = sort(filteredItems, args.sort);
+  const ids = diff ? filteredItems.map(it => it.id) : [];
+  const finalItems = diff ? items.map(it => !ids.includes(it.id) ? ({ ...it, notFound: true }): it) : filteredItems;
+  const sortedFilteredItems = diff ? finalItems : sort(finalItems, args.sort);
   return sortedFilteredItems.slice(args.start || 0, args.sortLimit || undefined);
 }
 
